d443bf5c6cb5139e8f813e93c2c2fa6172140fef
senthurveld <150390304+senthurveld@users.noreply.github.com.>
2026-01-20 21:27:42 +0530

    opacity

--- client/src/pages/EmailVerificationPage.jsx
+++ client/src/pages/EmailVerificationPage.jsx
@@ -65,12 +65,12 @@ const EmailVerificationPage = () => {
   }, [code]);
 
   return (
-    <div className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden">
+    <div className="max-w-md w-full bg-gray-800/50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden">
       <motion.div
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
-        className="bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-2xl p-8 w-full max-w-md"
+        className="bg-gray-800/50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-2xl p-8 w-full max-w-md"
       >
         <h2 className="text-3xl font-bold mb-6 text-center bg-linear-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
           Verify Your Email
