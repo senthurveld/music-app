4ebace81314c821e7211dbaf810f6b305cc46f27
senthurveld <150390304+senthurveld@users.noreply.github.com.>
2026-01-21 14:29:33 +0530

    config humburmenu

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import HamburgerMenu from "../components/HamburgerMenu";
 
 const HomePage = () => {
   const [menuOpen, setMenuOpen] = useState(false);
