496d2d324670ac8292320a4d1a481e5162897f7b
senthurveld <150390304+senthurveld@users.noreply.github.com.>
2026-01-21 14:41:34 +0530

    check auth update

---
+++ client/src/layouts/AppLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import { ThemeToggle } from "../components/Themetoggle";
+
+const AppLayout = () => {
+  return (
+    <div className="min-h-screen bg-black text-white relative">
+      {/* Theme Toggle Button */}
+      <ThemeToggle />
+
+      {/* Main content */}
+      <Outlet />
+    </div>
+  );
+};
+
+export default AppLayout;
